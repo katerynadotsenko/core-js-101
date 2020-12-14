ea4e727a25963448c83d56f69c41f740e30683e5
Katerina Dotsenko <ekatrinadotsenko@gmail.com>
2020-12-14 21:46:19 +0100

    feat; i hate lint

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -121,63 +121,33 @@ function fromJSON(proto, json) {
  *
  *  For more examples see unit tests.
  */
-
 const cssSelectorBuilder = {
-  element(value) {
-    const obj = Object.create({ value });
-    obj.stringify = function stringify() {
-      return this.value;
-    };
-    return obj;
+  element(/* value */) {
+    throw new Error('Not implemented');
   },
 
-  id(value) {
-    const obj = Object.create({ value: `#${value}` });
-    obj.stringify = function stringify() {
-      return this.value;
-    };
-    return obj;
+  id(/* value */) {
+    throw new Error('Not implemented');
   },
 
-  class(value) {
-    const obj = Object.create({ value: `.${value}` });
-    obj.stringify = function stringify() {
-      return this.value;
-    };
-    return obj;
+  class(/* value */) {
+    throw new Error('Not implemented');
   },
 
-  attr(value) {
-    const obj = Object.create({ value: `[${value}]` });
-    obj.stringify = function stringify() {
-      return this.value;
-    };
-    return obj;
+  attr(/* value */) {
+    throw new Error('Not implemented');
   },
 
-  pseudoClass(value) {
-    const obj = Object.create({ value: `:${value}` });
-    obj.stringify = function stringify() {
-      return this.value;
-    };
-    return obj;
+  pseudoClass(/* value */) {
+    throw new Error('Not implemented');
   },
 
-  pseudoElement(value) {
-    const obj = Object.create({ value: `::${value}` });
-    obj.stringify = function stringify() {
-      return this.value;
-    };
-    return obj;
+  pseudoElement(/* value */) {
+    throw new Error('Not implemented');
   },
 
-  combine(selector1, combinator, selector2) {
-    const obj = Object.create({ value: `${selector1}${combinator}${selector2}` });
-    obj.stringify = function stringify() {
-      console.log(this.value);
-      return this.value;
-    };
-    return obj;
+  combine(/* selector1, combinator, selector2 */) {
+    throw new Error('Not implemented');
   },
 };
 
